8a6c09eccf1d39b16eb9058b0e836a91607acdaf
Kassio Sousa <kassiomaximus10@gmail.com>
2022-03-17 10:58:05 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Kassio Sousa - Portfolio">
+    <meta name="keywords" content="HTML, CSS, JavaScript, PHP, C#, Games, Unity">
+    <meta name="author" content="Kassio Sousa">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kassio Sousa - Portfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="sidebar">
+        a
+      </div>
+
+      <div class="content">
+        b
+      </div>
+
+      <div class="footer">
+        c
+      </div>
+    </div>
+
+  </body>
+</html>
