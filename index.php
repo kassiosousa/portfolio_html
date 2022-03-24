7073a8490dff3c7026fbad8b98870703a5cb5628
Kassio Sousa <kassiomaximus10@gmail.com>
2022-03-24 11:10:04 -0300

    finishing first 3 cards of sidebar with flex

--- index.php
+++ index.php
@@ -14,16 +14,31 @@
 
     <sidebar>
       <div class="card profile">
-        <img class="card" src="img/profile.jpg">
+        <img src="img/profile.jpg">
         <h3>Kassio Sousa</h3>
         <h5>Fullstack Developer PHP/C#</h5>
       </div>
       <div class="card social">
-
-          <img class="card" src="favicons/briefcase.svg">
-
+        <a href="#" target="_blank" class="media"><img class="icon" src="favicons/github.svg"> kassiosousa</a>
+        <a href="#" target="_blank" class="media"><img class="icon" src="favicons/linkedin.svg"> kassiosousa</a>
+        <a href="#" target="_blank" class="media"><img class="icon" src="favicons/twitter.svg"> kassioromulo</a>
+        <a href="#" target="_blank" class="media"><img class="icon" src="favicons/globe.svg"> www.kassiosousa.com.br</a>
+        <a href="#" target="_blank" class="media"><img class="icon" src="favicons/map-pin.svg"> Brazil</a>
       </div>
-      <div class="card bio"> Bio </div>
+      <div class="card bio">
+        <div class="bio_text">
+          Graduated in Computer Science - UFMA (Universidade Federal do Maranhão).
+          Web developer specialist in PHP backend/frontend, since 2010, developing sites and web systems.
+          Beside PHP have works using C# and Python.
+          Enthusiast in game development, have founded Hardcoffee Game Studio in 2015. Participated in big games events (SbGames, Global game jam (GGJ), Big Festival and others game jams).
+
+          Local Organizer Global Game Jam - Maranhão
+
+          Founder at Reservaí (www.reservaí.com.br), a system to schedule services, made it for autonomous workers. This project was approved as the 300 top innovators projects in Brazil, by Innovativa acceleration program.
+
+          Co-founder at OPS GAME STUDIO in 2018, which its a company with focus to develop games to market demand and provide outsourcing.
+        </div>
+      </div>
       <div class="card abilities"> Habilidades </div>
       <div class="card education"> Formação </div>
     </sidebar>
@@ -36,9 +51,9 @@
     </container>
 
 
-    <!-- <footer>
+    <footer>
       Copyright @ Kassio Sousa 2022
-    </footer> -->
+    </footer>
 
   </body>
 </html>
