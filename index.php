1455c31b6f4850899a557787b31ab356228bc9ee
Kassio Sousa <kassiomaximus10@gmail.com>
2022-03-23 23:17:50 -0300

    initial commit

--- index.php
+++ index.php
@@ -4,25 +4,41 @@
     <meta name="description" content="Kassio Sousa - Portfolio">
     <meta name="keywords" content="HTML, CSS, JavaScript, PHP, C#, Games, Unity">
     <meta name="author" content="Kassio Sousa">
+    <link rel="icon" type="image/x-icon" href="favicons/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kassio Sousa - Portfolio</title>
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
 
-    <div class="container">
-      <div class="sidebar">
-        a
-      </div>
 
-      <div class="content">
-        b
+    <sidebar>
+      <div class="card profile">
+        <img class="card" src="img/profile.jpg">
+        <h3>Kassio Sousa</h3>
+        <h5>Fullstack Developer PHP/C#</h5>
       </div>
+      <div class="card social">
 
-      <div class="footer">
-        c
+          <img class="card" src="favicons/briefcase.svg">
+
       </div>
-    </div>
+      <div class="card bio"> Bio </div>
+      <div class="card abilities"> Habilidades </div>
+      <div class="card education"> Formação </div>
+    </sidebar>
+
+    <container>
+      <div class="card title">Experience</div>
+      <div class="card experience"> Experiencia </div>
+      <div class="card title">Projects</div>
+      <div class="card">Content 4</div>
+    </container>
+
+
+    <!-- <footer>
+      Copyright @ Kassio Sousa 2022
+    </footer> -->
 
   </body>
 </html>
