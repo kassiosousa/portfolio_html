a4f216385739f99390dd573bbf8407e3a2520a71
Kassio Sousa <kassiomaximus10@gmail.com>
2022-03-28 10:30:25 -0300

    768 screen finished

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
   </head>
   <body>
 
-
     <sidebar>
       <div class="card profile">
         <img src="img/profile.jpg">
@@ -63,10 +62,61 @@
         <div class="title_text">Experience</div>
         <div class="title_icon">See All <img class="icon" src="favicons/folder.svg"></div>
       </div>
-      <div class="card experience">
-          <div class="experience_card">
-            <h5>Computer Science Bachelor</h5>
-            <div class="year">2015</div>
+
+      <div class="experience">
+        <div class="card experience_card">
+          <h5>Fullstack Developer PHP</h5>
+          <div class="institute">UFMA</div>
+          <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2017-Now</div>
+          <div class="description">texto texto texto texto texto </div>
+          <div class="technologies">
+            <div class="tech">PHP</div>
+            <div class="tech">MYSQL</div>
+            <div class="tech">HTML</div>
+            <div class="tech">CSS</div>
+            <div class="tech">JS</div>
+          </div>
+        </div>
+        <div class="card experience_card">
+          <h5>C# Developer</h5>
+          <div class="institute">TWIGOAL</div>
+          <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2014-2016</div>
+          <div class="description">texto texto texto texto texto </div>
+          <div class="technologies">
+            <div class="tech">C#</div>
+            <div class="tech">ASP.NET</div>
+            <div class="tech">JS</div>
+            <div class="tech">SQLServer</div>
+            <div class="tech">HTML</div>
+            <div class="tech">SCRUM</div>
+          </div>
+        </div>
+        <div class="card experience_card">
+          <h5>PHP Developer</h5>
+          <div class="institute">TAGORY TI</div>
+          <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2012-2014</div>
+          <div class="description">ERP modules Development and users support </div>
+          <div class="technologies">
+            <div class="tech">PHP</div>
+            <div class="tech">MYSQL</div>
+            <div class="tech">HTML</div>
+            <div class="tech">CSS</div>
+            <div class="tech">JS</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card title">
+        <div class="title_text">Projects</div>
+        <div class="title_icon">See All <img class="icon" src="favicons/folder.svg"></div>
+      </div>
+      <div class="projects">
+
+          <div class="card project_card">
+            <img class="project_img" src="img/profile.jpg">
+            <h5><a href="#">Ops Game Studio</a></h5>
+            <div class="role">Programmer</div>
+            <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2018</div>
             <div class="description">texto texto texto texto texto </div>
             <div class="technologies">
               <div class="tech">HTML</div>
@@ -74,13 +124,56 @@
               <div class="tech">PHP</div>
             </div>
           </div>
-      </div>
+          <div class="card project_card">
+            <img class="project_img" src="img/profile.jpg">
+            <h5><a href="#">Ops Game Studio</a></h5>
+            <div class="role">Programmer</div>
+            <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2018</div>
+            <div class="description">texto texto texto texto texto </div>
+            <div class="technologies">
+              <div class="tech">HTML</div>
+              <div class="tech">CSS</div>
+              <div class="tech">PHP</div>
+            </div>
+          </div>
+          <div class="card project_card">
+            <img class="project_img" src="img/profile.jpg">
+            <h5><a href="#">Ops Game Studio</a></h5>
+            <div class="role">Programmer</div>
+            <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2018</div>
+            <div class="description">texto texto texto texto texto </div>
+            <div class="technologies">
+              <div class="tech">HTML</div>
+              <div class="tech">CSS</div>
+              <div class="tech">PHP</div>
+            </div>
+          </div>
+          <div class="card project_card">
+            <img class="project_img" src="img/profile.jpg">
+            <h5><a href="#">Ops Game Studio</a></h5>
+            <div class="role">Programmer</div>
+            <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2018</div>
+            <div class="description">texto texto texto texto texto </div>
+            <div class="technologies">
+              <div class="tech">HTML</div>
+              <div class="tech">CSS</div>
+              <div class="tech">PHP</div>
+            </div>
+          </div>
+          <div class="card project_card">
+            <img class="project_img" src="img/profile.jpg">
+            <h5><a href="#">Ops Game Studio</a></h5>
+            <div class="role">Programmer</div>
+            <div class="year"><img class="icon" src="favicons/briefcase.svg"> 2018</div>
+            <div class="description">texto texto texto texto texto </div>
+            <div class="technologies">
+              <div class="tech">HTML</div>
+              <div class="tech">CSS</div>
+              <div class="tech">PHP</div>
+            </div>
+          </div>
 
-      <div class="card title">
-        <div class="title_text">Projects</div>
-        <div class="title_icon">See All <img class="icon" src="favicons/folder.svg"></div>
       </div>
-      <div class="card">Projetos</div>
 
     </container>
 
